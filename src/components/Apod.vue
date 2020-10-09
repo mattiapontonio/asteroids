b7f9cddac3379b10ad7e82e423d586b372477983
Mattia Pontonio <mattia.pontonio@gmail.com>
2020-10-09 17:05:55 +0200

    apod

---
+++ src/components/Apod.vue
@@ -0,0 +1,58 @@
+<template>
+    <article>
+        <h2>Astronomy Picture of the Day</h2>
+        <div v-if="loading">Loading...</div>
+        <div v-else>
+            <p>{{date}}</p>
+            <picture>
+                <source
+                    media="(min-width:1680px)"
+                    v-bind:srcset="hdurl"
+                >
+                <source
+                    media="(min-width:465px)"
+                    v-bind:srcset="url"
+                >
+                <img
+                    loading="lazy"
+                    v-bind:src="url"
+                    v-bind:alt="title"
+                    v-bind:title="title"
+                >
+                <figcaption>{{ explanation }}</figcaption>
+            </picture>
+        </div>
+    </article>
+</template>
+<script>
+    import axios from 'axios';
+    export default {
+        name: 'apod',
+        data: function () {
+            return {
+                "date": undefined,
+                "explanation": undefined,
+                "hdurl": undefined,
+                "media_type": undefined,
+                "service_version": undefined,
+                "title": undefined,
+                "url": undefined,
+                loading: false
+            }
+        },
+        methods: {
+            get() {
+                this.loading = true;
+                this.errored = false;
+                axios.get('/planetary/apod').then(response => Object.assign(this, response.data)).catch(error => {
+                    console.log(error)
+                    this.errored = true
+                }).finally(() => this.loading = false);
+            }
+        },
+        mounted() {
+            this.get();
+        }
+    }
+
+</script>
