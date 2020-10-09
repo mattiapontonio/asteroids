a8a433d51122226b5e1e66d51447403186d0e027
Mattia Pontonio <mattia.pontonio@gmail.com>
2020-10-09 23:13:34 +0200

    varie...

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 const app = express()
 const port = process.env.PORT;
 const https = require('https');
 const querystring = require('querystring');
-app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/planetary/apod', (oreq, ores) => {
     const options = {
         host: 'api.nasa.gov',
