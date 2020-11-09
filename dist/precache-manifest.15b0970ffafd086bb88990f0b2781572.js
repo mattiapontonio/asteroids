a8cc8a2c6f36a5fe2eae345bb6e352b41b03b9b3
Mattia Pontonio <mattia.pontonio@gmail.com>
2020-11-09 17:44:23 +0100

    Current page responds with a 200 when offline | start_url responds with a 200 when offline

---
+++ dist/precache-manifest.15b0970ffafd086bb88990f0b2781572.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98fc1d7bf0e7d3dcb4a",
+    "url": "/css/app.25c76170.css"
+  },
+  {
+    "revision": "5c8c37d94e67f8173afda12a276f55a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d98fc1d7bf0e7d3dcb4a",
+    "url": "/js/app.30982ce1.js"
+  },
+  {
+    "revision": "79365bdc851eb4804f1d",
+    "url": "/js/chunk-vendors.cef04e62.js"
+  },
+  {
+    "revision": "d3d01307a0afb418f4b3992a8a6e2663",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
