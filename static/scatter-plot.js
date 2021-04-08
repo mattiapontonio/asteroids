903079827c7119986476ff4a63b1f9b4650a3f09
Mattia Pontonio <mattia.pontonio@gmail.com>
2021-04-08 22:05:06 +0200

    webcomponent

--- static/scatter-plot.js
+++ static/scatter-plot.js
@@ -2,9 +2,12 @@ customElements.define('scatter-plot', class extends HTMLElement {
     constructor() {
       super();
       fetch("https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-04-01&end_date=2021-04-01&api_key=DEMO_KEY")
-        .then(response => response.json())
+        .then(response => {
+            response.json();
+            console.table(response);
+        })
         .then(body => {
-          const s = document.createElement("svg");
+          const s = document.createElementNS("http://www.w3.org/2000/svg", "svg");
           const o = body.near_earth_objects["2021-04-01"].map(function(e, i, a) {
             return {
               id: e.id,
@@ -13,32 +16,35 @@ customElements.define('scatter-plot', class extends HTMLElement {
               r: e.estimated_diameter.kilometers.estimated_diameter_max
             }
           });
-          s.setAttribute("viewBox", [
+          s.setAttributeNS("http://www.w3.org/2000/svg","viewBox", [
             Math.min.apply(null, o.map(e => e.cx)),
             Math.min.apply(null, o.map(e => e.cy)),
             Math.max.apply(null, o.map(e => e.cx)),
             Math.max.apply(null, o.map(e => e.cy))
           ].join(" "));
-          s.setAttribute("height", "100");
-          s.setAttribute("width", "100");
-          s.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-          s.setAttribute("stroke", "red");
-          s.setAttribute("fill", "grey");
+          s.setAttributeNS("http://www.w3.org/2000/svg","height", "100");
+          s.setAttributeNS("http://www.w3.org/2000/svg","width", "100");
+          s.setAttributeNS("http://www.w3.org/2000/svg","stroke", "red");
+          s.setAttributeNS("http://www.w3.org/2000/svg","fill", "grey");
           s.style.setProperty('display', "block");
           s.style.setProperty('width', "auto");
           s.style.setProperty('height', "300px");
           o.map(e => {
-            const c = document.createElement("circle");
-            c.setAttribute("id", e.id);
-            c.setAttribute("cx", e.cx);
-            c.setAttribute("cy", e.cy);
-            c.setAttribute("r", e.r);
-            c.setAttribute("stroke", "black");
-            c.setAttribute("stroke-width", "3");
-            c.setAttribute("fill", "red");
+            const c = document.createElementNS("http://www.w3.org/2000/svg","circle");
+            c.setAttributeNS("http://www.w3.org/2000/svg","id", e.id);
+            c.setAttributeNS("http://www.w3.org/2000/svg","cx", e.cx);
+            c.setAttributeNS("http://www.w3.org/2000/svg","cy", e.cy);
+            c.setAttributeNS("http://www.w3.org/2000/svg","r", 50);
+            c.setAttributeNS("http://www.w3.org/2000/svg","stroke", "black");
+            c.setAttributeNS("http://www.w3.org/2000/svg","stroke-width", "3");
+            c.setAttributeNS("http://www.w3.org/2000/svg","fill", "red");
             s.appendChild(c)
           });
-          this.appendChild(s)
+          this.innerHTML=`
+            <div>${response.statusText}</div>
+            ${s.outerHTML}
+          `;
         })
+        .catch(e=> this.innerHTML=e)
     }
   });
