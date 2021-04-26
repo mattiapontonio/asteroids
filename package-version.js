d92a4eda825a37a95c6987c200df1e8479305bdf
Mattia Pontonio <mattia.pontonio@gmail.com>
2021-04-26 16:09:13 +0200

    package-version

---
+++ package-version.js
@@ -0,0 +1,10 @@
+customElements.define(
+  'package-version',
+  class extends HTMLElement {
+    connectedCallback() {
+      fetch('./package.json')
+        .then((response) => response.json())
+        .then((data) => (this.innerText = data.version))
+    }
+  }
+)
