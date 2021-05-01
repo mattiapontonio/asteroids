a7ed0ab7ebc38b39f50741131e86d5b15cc0715c
Mattia Pontonio <mattia.pontonio@gmail.com>
2021-05-01 15:55:34 +0200

    ux

--- nav.js
+++ nav.js
@@ -7,13 +7,14 @@ export default customElements.define(
         connectedCallback() {
             this.innerHTML = `
             <nav>
-                <a href="/index.html?api_key=${this.api_key}">index</a>
-                <a href="/bubble-chart.html?api_key=${this.api_key}"">bubble-chart</a>
-                <a href="/apod.html?api_key=${this.api_key}"">apod</a>
-                <a href="/asteroid.html?api_key=${this.api_key}"">asteroid</a>
-                <a href="/login.html?api_key=${this.api_key}"">
-                <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/login/v10/24px.svg"/>
-                login</a>
+                <h2>Navigation</h2> 
+                <a href="/index.html?api_key=${this.api_key}"><img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/home/v11/24px.svg"/>index</a>
+                <a href="/bubble-chart.html?api_key=${this.api_key}">bubble-chart</a>
+                <a href="/apod.html?api_key=${this.api_key}">apod</a>
+                <a href="/asteroid.html?api_key=${this.api_key}">asteroid</a>
+                <a href="/login.html?api_key=${this.api_key}"><img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/login/v10/24px.svg"/>login</a>
+                <a href="/logout.html?api_key=${this.api_key}"><img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/logout/v10/24px.svg"/>logout</a>
+                <a href="/settings.html?api_key=${this.api_key}"><img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/settings/v14/24px.svg"/>settings</a>
             </nav>`
         }
     }
