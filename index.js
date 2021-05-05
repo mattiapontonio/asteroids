4c4833151d9f0e08c1c868161d8cbeeb3e1e507a
Mattia Pontonio <mattia.pontonio@gmail.com>
2021-05-05 18:59:54 +0200

    no-cache

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const static = require('node-static');
 const http = require('http');
-const file = new(static.Server)(__dirname);
-http.createServer(function (req, res) {
-  file.serve(req, res);
-}).listen(process.env.PORT||8080);
+const file = new static.Server(__dirname, {
+    cache: 0,
+    headers: { 'Cache-Control': 'no-cache' },
+    gzip: true
+});
+http.createServer(function(req, res) {
+    file.serve(req, res);
+}).listen(process.env.PORT || 8080);
